53f8d2969f5f216e29dd51cced8f799ddf22100b
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-03 12:06:03 +0200

    keep a safe initializer possibility, if we want prog and sugg to be equals, but default to requiring prog to always be set higher than sugg

--- Sources/Economics/pricing/session-estimation.swift
+++ Sources/Economics/pricing/session-estimation.swift
@@ -3,13 +3,16 @@ import Foundation
 public enum SessionCountEstimationError: Error, LocalizedError, Sendable {
     case localExceedsCount(count: Int, local: Int)
     case suggestionExceedsPrognosis(prognosis: Int, suggestion: Int)
+    case prognosisNotHigherThanSuggestion(prognosis: Int, suggestion: Int)
 
     public var errorDescription: String? {
         switch self {
         case let .localExceedsCount(count, local):
             return "Invalid SessionCountEstimationObject: local (\(local)) cannot exceed count (\(count))."
         case let .suggestionExceedsPrognosis(prognosis, suggestion):
-            return "Invalid SessionCountEstimationObject: prognosis (\(prognosis)) cannot exceed count (\(suggestion))."
+            return "Invalid SessionCountEstimationObject: prognosis (\(prognosis)) cannot exceed suggestion (\(suggestion))."
+        case let .prognosisNotHigherThanSuggestion(prognosis, suggestion):
+            return "Invalid SessionCountEstimationObject: prognosis (\(prognosis)) must exceed suggestion (\(suggestion))."
         }
     }
 }
@@ -46,19 +49,45 @@ public struct SessionCountEstimationObject: Sendable {
     }
 }
 
+public enum SessionCountEstimationInitializerRestriction: Sendable {
+    case equal
+    case incremental
+
+    public var explanation: String {
+        switch self {
+            case .equal:
+                return "The 'suggestion' session count can not exceed the 'prognosis' session count, but they can be equal."
+            case .incremental:
+                return "The 'prognosis' session count must be set higher than the 'suggestion' session count; they cannot be equal."
+        }
+    }
+}
+
 public struct SessionCountEstimation: Sendable {
     public let prognosis: SessionCountEstimationObject
     public let suggestion: SessionCountEstimationObject
+    public let restriction: SessionCountEstimationInitializerRestriction
 
     public init(
         prognosis: SessionCountEstimationObject,
         suggestion: SessionCountEstimationObject,
+        restriction: SessionCountEstimationInitializerRestriction = .incremental
     ) throws {
-        guard !(suggestion.count > prognosis.count) else {
-            throw SessionCountEstimationError.suggestionExceedsPrognosis(
-                prognosis: prognosis.count,
-                suggestion: suggestion.count
-            )
+        self.restriction = restriction
+        if restriction == .equal {
+            guard !(suggestion.count > prognosis.count) else {
+                throw SessionCountEstimationError.suggestionExceedsPrognosis(
+                    prognosis: prognosis.count,
+                    suggestion: suggestion.count
+                )
+            }
+        } else {
+            guard prognosis.count > suggestion.count else {
+                throw SessionCountEstimationError.prognosisNotHigherThanSuggestion(
+                    prognosis: prognosis.count,
+                    suggestion: suggestion.count
+                )
+            }
         }
         self.prognosis = prognosis
         self.suggestion = suggestion
