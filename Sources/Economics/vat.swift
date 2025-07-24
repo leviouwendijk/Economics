c89f5a1ffab063648cb0e2e262b8a017ae2f3e9f
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-24 16:19:56 +0200

    vat double calcing

--- Sources/Economics/vat.swift
+++ Sources/Economics/vat.swift
@@ -8,7 +8,7 @@ public enum StandardVAT {
     case norway, croatia, denmark, sweden
     case portugal, poland
 
-    var rate: Int {
+    var rate: Double {
         switch self {
         case .netherlands, .belgium: return 21
         case .unitedKingdom, .france: return 20
@@ -20,7 +20,7 @@ public enum StandardVAT {
     }
 }
 
-public func standardVAT(_ country: StandardVAT) -> Int {
+public func standardVAT(_ country: StandardVAT) -> Double {
     return country.rate
 }
 
@@ -30,36 +30,34 @@ public enum VAT {
 }
 
 public protocol ValueAddedTaxableDouble {
-    func vat(_ vatRate: Int, _ calculatedValue: VAT) -> Double
+    func vat(_ vatRate: Double, _ calculatedValue: VAT) -> Double
 }
 
 public protocol ValueAddedTaxableInt {
-    func vat(_ vatRate: Int, _ calculatedValue: VAT) -> Int
+    func vat(_ vatRate: Double, _ calculatedValue: VAT) -> Int
 }
 
 extension Double: ValueAddedTaxableDouble {
-    public func vat(_ vatRate: Int = standardVAT(.netherlands), _ calculatedValue: VAT = .vat) -> Double {
-        let vatRateDouble = Double(vatRate)
+    public func vat(_ vatRate: Double = standardVAT(.netherlands), _ calculatedValue: VAT = .vat) -> Double {
         switch calculatedValue {
         case .vat:
-            return (self / (100.0 + vatRateDouble)) * vatRateDouble
+            return (self / (100.0 + vatRate)) * vatRate
         case .revenue:
-            return (self / (100.0 + vatRateDouble)) * 100.0
+            return (self / (100.0 + vatRate)) * 100.0
         }
     }
 }
 
 extension Int: ValueAddedTaxableInt {
-    public func vat(_ vatRate: Int = standardVAT(.netherlands), _ calculatedValue: VAT = .vat) -> Int {
+    public func vat(_ vatRate: Double = standardVAT(.netherlands), _ calculatedValue: VAT = .vat) -> Int {
         let doubleValue = Double(self)
-        let vatRateDouble = Double(vatRate)
         let result: Double
         
         switch calculatedValue {
         case .vat:
-            result = (doubleValue / (100.0 + vatRateDouble)) * vatRateDouble
+            result = (doubleValue / (100.0 + vatRate)) * vatRate
         case .revenue:
-            result = (doubleValue / (100.0 + vatRateDouble)) * 100.0
+            result = (doubleValue / (100.0 + vatRate)) * 100.0
         }
         
         return Int(result.rounded())
